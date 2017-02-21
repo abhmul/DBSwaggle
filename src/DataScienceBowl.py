7611dc77edfa2a75a58206a9bd7237b59630eeb0
Unknown <aam14@rice.edu>
2017-02-20 18:26:34 -0600

    added load img function
    a

--- src/DataScienceBowl.py
+++ src/DataScienceBowl.py
@@ -1,1 +1,17 @@
 import dicom
+import numpy as np
+
+
+# Load the scans in given folder path
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: int(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
