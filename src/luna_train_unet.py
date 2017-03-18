f65f1976ce1da36502a3d5d2cd48e97ede06861c
Jack Duryea <jackduryea@gmail.com>
2017-03-18 15:54:46 -0500

    Added dice_coef functions

--- src/luna_train_unet.py
+++ src/luna_train_unet.py
@@ -16,6 +16,23 @@ img_cols = 512
 smooth = 1.
 
 
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_coef_np(y_true,y_pred):
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+
 def get_unet():
     inputs = Input((1,img_rows, img_cols))
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
